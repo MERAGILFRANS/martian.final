e2800ec9cc0d61c026e22817c73a3d0b34e6b6a2
frans nicholastio <fransnicholas208@gmail.com>
2024-09-24 09:12:41 +0700

    update

--- start.sh
+++ start.sh
@@ -1,19 +1,22 @@
+#!/bin  /bash
 set -eu
 
 export PYTHONUNBUFFERED=true
 
 VIRTUALENV=.data/venv
 
-if [ ! -d $VIRTUALENV/bin/pip ]; then
-python3 -m venv $VIRTUALENV
+# Membuat virtual environment jika belum ada
+if [ ! -d "$VIRTUALENV" ]; then
+    python3 -m venv $VIRTUALENV
 fi
 
-if [ ! -f $VIRTUALENV/bin/pip]; then 
- curl  --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
- fi
+# Menginstal pip jika tidak ada
+if [ ! -f "$VIRTUALENV/bin/pip" ]; then
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+fi
 
+# Menginstal dependensi dari requirements.txt
 $VIRTUALENV/bin/pip install -r requirements.txt
 
-$VIRTUALENV/bin/python3 app.py  
-
-Footer
+# Menjalankan aplikasi
+$VIRTUALENV/bin/python app.py
